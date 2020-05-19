8c0da7b5a0b83c63ea76b065c314d44b20869af5
rlikeone <info@vbel.nl>
2020-05-19 02:53:27 +0200

    viewproject logic added

--- src/components/main-projects/ViewProject.js
+++ src/components/main-projects/ViewProject.js
@@ -4,23 +4,18 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 export const ViewProject = ({ projectLink, githubLink }) => {
-  const showGithub = (link) => {
-    if (link) {
-      return (
-        <a href={githubLink} rel="noopener noreferrer nofollow" target="_blank">
-          Github
-        </a>
-      );
-    }
-  };
-
   const { t } = useTranslation();
   return (
     <div className="listprojects__card__content__buttons">
       <a href={projectLink} rel="noopener noreferrer nofollow" target="_blank">
         {t("projects.view")}
       </a>
-      {showGithub(githubLink)}
+      {/* {showGithub(githubLink)} */}
+      {githubLink && (
+        <a href={githubLink} rel="noopener noreferrer nofollow" target="_blank">
+          Github
+        </a>
+      )}
     </div>
   );
 };
